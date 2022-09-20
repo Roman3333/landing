be119505bfa872260657b3a800a8e779a84e0569
Roman <rosipenko@yandex.ru>
2022-09-20 21:51:58 +0500

    fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Media from 'react-media';
+
 import Services from '../components/Services';
 import Portfolio from '../components/Portfolio';
 import Price from '../components/Price';
@@ -6,10 +8,27 @@ import About from '../components/About';
 import Form from '../components/Form';
 
 const Home = ({ setPopupVisible }) => {
+  // const screen1162 = window.matchMedia('(max-width: 1162px)');
+  // const screen729 = window.matchMedia('(max-width: 729px)');
+
   return (
     <>
       <Services />
-      <Portfolio itemsPerPage={6} />
+      {/* <Portfolio itemsPerPage={6} /> */}
+      <Media
+        queries={{
+          small: '(max-width: 729px)',
+          medium: '(min-width: 729px) and (max-width: 1162px)',
+          large: '(min-width: 1200px)',
+        }}>
+        {(matches) => (
+          <>
+            {matches.small && <Portfolio itemsPerPage={2} />}
+            {matches.medium && <Portfolio itemsPerPage={4} />}
+            {matches.large && <Portfolio itemsPerPage={6} />}
+          </>
+        )}
+      </Media>
       <Price setPopupVisible={setPopupVisible} />
       <About />
       <Form />
